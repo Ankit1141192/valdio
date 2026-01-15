862ab07ddaebf187eb57d70506a1e5b7eb0a0e3a
Ankit Kumar <ankit2914978@gmail.com>
2026-01-15 18:51:05 +0530

    rjn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 
 createRoot(document.getElementById('root')).render(
@@ -10,7 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       
      <App/>
-    
+     <SpeedInsights />
     </BrowserRouter>
     
   </StrictMode>,
